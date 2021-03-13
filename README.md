0cf3ead2ca389731943186c12538fe8ebfa36202
Amon0412 <gabrielechiarenza0412@gmail.com>
2021-03-13 21:00:10 -0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tofe4n-ihkrrv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tofe4n-ihkrrv)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Phone XL",
+    price: 799,
+    description: "Un telefono grande con uno dei migliori schermi"
+  },
+  {
+    id: 2,
+    name: "Phone Mini",
+    price: 699,
+    description: "Un ottimo telefono con una delle migliori fotocamere"
+  },
+  {
+    id: 3,
+    name: "Phone Standard",
+    price: 299,
+    description: "normale iphon, una merda"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
